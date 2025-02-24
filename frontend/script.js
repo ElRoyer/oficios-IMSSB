1dc3cfc542203f58d88460203aa7869879d38fb1
ElRoyer <66813393+ElRoyer@users.noreply.github.com>
2025-02-24 17:21:27 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ document.getElementById("loginForm").addEventListener("submit", async function (
     const errorMensaje = document.getElementById("errorMensaje");
   
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://oficios-imssb.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ usuario, contrasenia }),
