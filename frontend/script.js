09591a080961c8cde68d63833bc50c3643319bdc
ElRoyer <66813393+ElRoyer@users.noreply.github.com>
2025-02-28 14:52:38 -0600

    Update script.js eliminacion de barra

--- frontend/script.js
+++ frontend/script.js
@@ -8,14 +8,6 @@ document
     const errorMensaje = document.getElementById("errorMensaje");
     const successMensaje = document.getElementById("successMensaje");
 
-    // Si la barra no existe, la creamos
-    let progressBar = document.getElementById("progressBar");
-    if (!progressBar) {
-      progressBar = document.createElement("div");
-      progressBar.id = "progressBar";
-      errorMensaje.appendChild(progressBar);
-    }
-
     try {
       const response = await fetch("https://oficios-imssb.onrender.com/login", {
         method: "POST",
@@ -42,24 +34,11 @@ document
       } else {
         errorMensaje.textContent = data.error;
         errorMensaje.style.display = "block";
-
-        // Resetear barra de progreso
-        progressBar.style.width = "100%";
-        setTimeout(() => {
-          errorMensaje.style.display = "none";
-          contrasenia = set;
-        }, 3000);
       }
     } catch (error) {
       console.error("Error en la solicitud:", error);
       errorMensaje.textContent = "Error al conectar con el servidor";
       errorMensaje.style.display = "block";
-
-      // Misma animación para el error de conexión
-      progressBar.style.width = "100%";
-      setTimeout(() => {
-        progressBar.style.width = "0%";
-      }, 50);
 
       setTimeout(() => {
         errorMensaje.style.display = "none";
